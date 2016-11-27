fdb87b1203d8f32f8268a95724b55111691ee14e
MaverickHitesh <hitesh.135288@gmail.com>
2016-11-27 16:23:41 +0530

    pushing initial source code

---
+++ iCareer/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  iCareer
+//
+//  Created by Hitesh Kumar Singh on 27/11/16.
+//  Copyright © 2016 Hitesh Kumar Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ iCareer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iCareer
+//
+//  Created by Hitesh Kumar Singh on 27/11/16.
+//  Copyright © 2016 Hitesh Kumar Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
