6c0ebbfa854daf9f280ca5f77ae16602847c566e
Hitesh <Hitesh@maverick.local>
2016-12-01 17:41:10 +0530

    HCSStarRatingView added to display stars

--- iCareer/View/ResultVC.m
+++ iCareer/View/ResultVC.m
@@ -7,8 +7,10 @@
 //
 
 #import "ResultVC.h"
+#import "HCSStarRatingView.h"
 
 @interface ResultVC ()
+@property (weak, nonatomic) IBOutlet HCSStarRatingView *starView;
 
 @end
 
@@ -16,7 +18,13 @@
 #pragma mark - view life cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    self.starView.maximumValue = 5;
+    self.starView.minimumValue = 1;
+    self.starView.value = 0;
+    self.starView.tintColor = [UIColor colorWithRed:255.0/255.0 green:151.0/255.0 blue:61.0/255.0 alpha:1.0f];
+    self.starView.allowsHalfStars = true;
+    self.starView.accurateHalfStars = true;
+    self.starView.value = 3.3f;
 }
 - (IBAction)explore:(id)sender {
     
